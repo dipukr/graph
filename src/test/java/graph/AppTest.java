e606c1a22ad87d6640519d893a5054ed1c81cebd
adipukr <dipu.kumar@50hertz.in>
2025-01-30 18:38:02 +0530

    .

--- src/test/java/graph/AppTest.java
+++ src/test/java/graph/AppTest.java
@@ -1,19 +1,5 @@
 package graph;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import org.junit.jupiter.api.Test;
-
-/**
- * Unit test for simple App.
- */
 public class AppTest {
 
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
-    }
 }
